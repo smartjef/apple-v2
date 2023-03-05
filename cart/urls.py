78d5674ecbbd9590eb9c7a1102733a521d1f998d
JEFF ODHIAMBO <jeff.odhiambo@studentambassadors.com>
2023-03-05 14:48:28 +0300

    done

--- cart/urls.py
+++ cart/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('detailed/', views.cart_detail, name='cart_detail'),
     path('wish/detailed/', views.wish_detailed, name='wish_detail'),
     path('add/<int:product_id>/', views.cart_add, name='cart_add'),
-    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
     path('wish/add/<int:product_id>/', views.wish_add, name='wish_add'),
     path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
     path('wish/remove/<int:product_id>/', views.wish_remove, name='wish_remove'),
--- orders/urls.py
+++ orders/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('make-payment/', views.make_manual_payment, name='make_manual_payment'),
     path('history/', views.order_history, name='order_history'),
     path('<int:order_id>/', views.view_order_details, name='order_details'),
-    path('<int:order_id>/invoice', views.view_invoice, name='order_invoice'),
+    path('<int:order_id>/invoice/', views.view_invoice, name='order_invoice'),
     path('<int:order_id>/transact/', views.create_transaction, name='create_transaction'),
-    path('<int:order_id>/payment', views.make_payment, name='make_payment'),
-    path('<int:order_id>/payment/history', views.order_payment_history, name='order_payment_history'),
+    path('<int:order_id>/payment/', views.make_payment, name='make_payment'),
+    path('<int:order_id>/payment/history/', views.order_payment_history, name='order_payment_history'),
 ]
